894df6c61dd9c6fdeeeba2bd235ab6820bdbbc27
dmorina <drm.mrn@gmail.com>
2016-04-27 16:32:07 -0700

    add get_projects

--- daemo/client.py
+++ daemo/client.py
@@ -1,16 +1,25 @@
 import requests
 
 
-class DaemoClient():
+class DaemoClient:
     def __init__(self, host, access_token):
         self.host = host
         self.access_token = access_token
         self.session = requests.session()
 
-    def request(self, path, data):
+    def post_request(self, path, data):
         headers = {"Authorization": "Bearer " + self.access_token, "Content-Type": 'application/json'}
         response = self.session.post(self.host + path, data=data, headers=headers)
         return response
 
+    def get_request(self, path):
+        self.session.headers.update({"Authorization": "Bearer " + self.access_token})
+        response = self.session.get(url=self.host + path)
+        return response
+
     def create_project(self, data):
-        return self.request(path='api/project/create-full/', data=data)
+        return self.post_request(path='api/project/create-full/', data=data)
+
+    def get_projects(self):
+        return self.get_request('api/project/requester_projects/')
+
