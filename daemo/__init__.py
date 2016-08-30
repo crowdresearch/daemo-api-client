7a22c93c30024f5200182fc2c5bc276d66cff4d3
Shirish Goyal <shirish.goyal@gmail.com>
2016-08-30 11:45:30 -0700

    Client changes for protocol

--- daemo/__init__.py
+++ daemo/__init__.py
@@ -3,9 +3,6 @@ import logging
 __version__ = "0.0.1"
 
 HOST = "daemo.stanford.edu"
-# HOST = "127.0.0.1:8000"
-HTTP = "https://"
-WEBSOCKET = "wss://"
 WS_BOT_SUBSCRIBE_URL = "/ws/bot?subscribe-user"
 OAUTH_TOKEN_URL = "/api/oauth2-ng/token/"
 
